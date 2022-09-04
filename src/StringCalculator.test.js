d1f663696b0d281dbcad0a27fb3266ce8e527c72
Kunj Patel <patelkunj699@gmail.com>
2022-09-04 15:39:58 +0530

    Task 2: Allow the add method to handle an unknown amount of numbers

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -1,24 +1,34 @@
 const StringCalculator = require("./StringCalculator");
 
-describe("String Calculator Task 1", () => {
-  beforeEach(() => {
-    const { add } = StringCalculator;
-    this.add = add;
+describe("String Calculator", () => {
+  describe("String Calculator Task 1", () => {
+    beforeEach(() => {
+      const { add } = StringCalculator;
+      this.add = add;
+    });
+
+    it("add() with empty string should return 0", () => {
+      expect(this.add("")).toBe(0);
+    });
+
+    it("add() with one numbers's string should return the number itself", () => {
+      expect(this.add("3")).toBe(3);
+    });
+
+    it("add() with two numbers's string should return sum of two numbers", () => {
+      expect(this.add("3,4")).toBe(3 + 4);
+    });
+
+    it("defines add()", () => {
+      expect(typeof this.add).toBe("function");
+    });
   });
 
-  it("add() with empty string should return 0", () => {
-    expect(this.add("")).toBe(0);
-  });
-
-  it("add() with one numbers string should return the number itself", () => {
-    expect(this.add("3")).toBe(3);
-  });
-
-  it("add() with two numbers string should return sum of two numbers", () => {
-    expect(this.add("3,4")).toBe(7);
-  });
-
-  it("defines add()", () => {
-    expect(typeof this.add).toBe("function");
+  describe("String Calculator Task 2", () => {
+    it("add() with unknown amount of number's string should return sum of all numbers", () => {
+      expect(StringCalculator.add("4,2,54,26,2,4,6,2,12")).toBe(
+        4 + 2 + 54 + 26 + 2 + 4 + 6 + 2 + 12
+      );
+    });
   });
 });
